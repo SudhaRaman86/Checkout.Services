beae757a945147983348bce8118863ebe5b03532
sudha <rpas.rsudha@gmail.com>
2023-11-20 20:41:13 -0500

    Add project files.

---
+++ Checkout.Services/Controllers/CheckoutController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Checkout.Services.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CheckoutController : Controller
+    {
+        private readonly ILogger<CheckoutController> _logger;
+        public CheckoutController(ILogger<CheckoutController> logger )
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public int Checkout()
+        {
+            int price = 100;
+            return price;
+        }
+    }
+}
